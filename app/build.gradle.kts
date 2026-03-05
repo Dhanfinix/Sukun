6ca40b6300b2adfabb1b75e4241b81ef959b07d5
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-03-05 13:24:00 +0700

    fix(release): bump version code to 18 to fix play console duplicate code error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "dhanfinix.android.sukun"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
+        versionCode = 18
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
