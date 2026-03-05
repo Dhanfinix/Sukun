ef039b3d5daf39815074fb9aa7d2b8d42c4edb3f
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-03-05 12:58:55 +0700

    chore: adjust version to 1.1.0 (17) and shorten release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dhanfinix.android.sukun"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.2.0"
+        versionCode = 17
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
