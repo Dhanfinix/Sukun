4bf311c9ef8bb04a1e7e0c4a4052e18f50ff8e7f
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-03-06 08:34:21 +0700

    build: bump versionName to 1.1.1 for production

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 20
-        versionName = "1.1.0"
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
