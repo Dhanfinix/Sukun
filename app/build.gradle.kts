12cbe8f6fb41e442441178e5512f2b9b2f617cec
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-03-08 14:42:02 +0700

    chore: bump versionCode to 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "dhanfinix.android.sukun"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
+        versionCode = 24
         versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
