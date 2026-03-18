d24ad4d2efe6de45254251175320585d41fb8927
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-03-18 13:23:39 +0700

    chore: bump versionCode to 35 and update release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "dhanfinix.android.sukun"
         minSdk = 26
         targetSdk = 36
-        versionCode = 34
+        versionCode = 35
         versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
