7988e02c6228c5c922e0678432b9a64b0e7fcce7
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-03-19 13:37:59 +0700

    fix: add javaScriptCanOpenWindowsAutomatically to allow window.open to reach onCreateWindow

--- app/src/main/java/dhanfinix/android/sukun/feature/webview/WebViewScreen.kt
+++ app/src/main/java/dhanfinix/android/sukun/feature/webview/WebViewScreen.kt
@@ -119,6 +119,7 @@ fun WebViewScreen(
                         settings.javaScriptEnabled = true
                         settings.domStorageEnabled = true
                         settings.databaseEnabled = true
+                        settings.javaScriptCanOpenWindowsAutomatically = true
                         settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
                         settings.userAgentString = "Mozilla/5.0 (Linux; Android 13; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36"
                         
