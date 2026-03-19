71ac85678ae5b7b98cc449f46100eb39e97acb76
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-03-19 14:11:19 +0700

    release: bump versionCode to 36, update whatsnew for v1.1.5 (36)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "dhanfinix.android.sukun"
         minSdk = 26
         targetSdk = 36
-        versionCode = 35
+        versionCode = 36
         versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
