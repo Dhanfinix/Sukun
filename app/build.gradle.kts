959fc79219d484aae0930106e6ad7583797d3cd6
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-03-27 09:11:43 +0700

    chore: bump versionCode to 38 and update whatsnew for instant geofence release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "dhanfinix.android.sukun"
         minSdk = 26
         targetSdk = 36
-        versionCode = 37
+        versionCode = 38
         versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
