6e766808a78a462a035bbe400cd6cd74caaf31ac
Muhammad <dhanfinix@github.com>
2026-04-15 21:19:00 +0800

    Prepare for internal testing: bump version and update internal pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dhanfinix.android.sukun"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.1.4"
+        versionCode = 45
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
