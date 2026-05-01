ed9e703e652143432d25198987965dcd0b2bd8e8
Muhammad Ramdhan <muhammad.ramdhan@sg-edts.com>
2026-05-01 10:13:27 +0700

    chore: prepare internal release 1.2.1 (49)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "dhanfinix.android.sukun"
         minSdk = 26
         targetSdk = 36
-        versionCode = 48
+        versionCode = 49
         versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
